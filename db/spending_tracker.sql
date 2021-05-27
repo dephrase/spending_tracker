a7c630e3c5ed032f9ff8347eddda03412f9aff02
Dephrase <davidjf123@gmail.com>
2021-05-27 18:06:28 +0100

    Basic Folder Structure Created

---
+++ db/spending_tracker.sql
@@ -0,0 +1,21 @@
+DROP TABLE IF EXISTS transactions;
+DROP TABLE IF EXISTS merchants;
+DROP TABLE IF EXISTS tags;
+
+CREATE TABLE tags (
+    id SERIAL PRIMARY KEY,
+    tag_name VARCHAR(255)
+)
+
+CREATE TABLE merchants (
+    id SERIAL PRIMARY KEY,
+    merchant_name VARCHAR(255)
+)
+
+CREATE TABLE transactions (
+    id SERIAL PRIMARY KEY,
+    transaction_name VARCHAR(255),
+    tag_id INT REFERENCES tags(id),
+    merchant_id INT REFERENCES merchants(id),
+    amount_spent DECIMAL(5,2)
+)
