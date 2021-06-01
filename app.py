2f27a08a9a9f45ca2c2eaf863c42307c1edc8cab
Dephrase <davidjf123@gmail.com>
2021-06-01 13:57:32 +0100

    Added basic styling to most pages

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ import repositories.transaction_repository as transaction_repository
 import repositories.merchant_repository as merchant_repository
 import repositories.tag_repository as tag_repository
 
+import matplotlib.pyplot as plt
+import numpy as np
+
 
 app = Flask(__name__)
 
@@ -42,9 +45,6 @@ def home():
         amountspent = transaction_repository.get_total_of_list(listoftrans)
         merch_dict[merchant.merchant_name] = amountspent
 
-
-
-    
     return render_template('index.html', total_spending=total_spending, total_transactions=total_transactions, frequent_merchant_name=frequent_merchant_name, frequent_merchant_visits=frequent_merchant_visits, most_expensive_transaction=most_expensive_transaction, frequent_tag_purchases=frequent_tag_purchases, frequent_tag_name=frequent_tag_name, tags=tags, spent_dict=spent_dict, merch_dict=merch_dict)
 
 if __name__ == '__main__':
